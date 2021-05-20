cc128a93980223ae7c37d980582a01885a89de87
Dorian Stoll <dorian.stoll@tmsp.io>
2021-05-20 07:12:07 +0200

    Pull in the github build scripts from iptsd
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

---
+++ .github/scripts/pkg-fedora.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+	$0 install
+	$0 build
+	$0 sign
+	$0 release
+	exit
+fi
+
+case "$1" in
+install)
+	# Setup build environment
+	dnf distro-sync -y
+	dnf install -y rpmdevtools rpm-sign rpkg 'dnf-command(builddep)'
+
+	# Install package dependencies
+	dnf builddep -y *.spec
+	;;
+build)
+	mkdir rpm
+
+	# Make sure that we have a git repository
+	if [ ! -d ".git" ]; then
+		git init
+	fi
+
+	rpkg local --outdir $PWD/rpm
+	;;
+sign)
+	if [ -z "$GPG_KEY" ] || [ -z "$GPG_KEY_ID" ]; then
+		echo "WARNING: No GPG key configured, skipping signing."
+		exit
+	fi
+
+	# import GPG key
+	echo "$GPG_KEY" | base64 -d | gpg --import --no-tty --batch --yes
+	export GPG_TTY=$(tty)
+
+	# sign package
+        rpm --resign rpm/**/*.rpm --define "_gpg_name $GPG_KEY_ID"
+	;;
+release)
+	mkdir -p release
+	mv rpm/**/*.rpm release
+	;;
+esac
