88f4ba80bf7598ca570390d8400932724b229b79
Maximilian Luz <luzmaximilian@gmail.com>
2021-10-20 19:44:06 +0200

    pkg/arch: Ignore dependencies
    The package depends on the linux-surface kernel package. This is
    somewhat hacky as the patched kernel is required for the workaround to
    work and we use this as mechanism to encourage people to install that.
    It is not required during build time.
    
    Installing the patched kernel would require adding the repo and since we
    don't need it at packaging time, let's just ignore dependencies and skip
    all that.

--- .github/scripts/pkg-arch.sh
+++ .github/scripts/pkg-arch.sh
@@ -24,7 +24,7 @@ build)
 	export COMPRESSZST=(zstd -c -T0 --ultra -20 -)
 
 	# Build
-	su nobody --pty -p -s /bin/bash -c 'makepkg -sf --noconfirm'
+	su nobody --pty -p -s /bin/bash -c 'makepkg -sdf --noconfirm'
 	;;
 sign)
 	if [ -z "$GPG_KEY" ] || [ -z "$GPG_KEY_ID" ]; then
